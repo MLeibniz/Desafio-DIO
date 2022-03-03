12a4172940c714c6d99f48933661bac026eb9f3c
Marcos <marcos.leibniz@gmail.com>
2022-03-03 19:52:52 -0300

    name replacement

--- ConsoleApp/Aula2.cs
+++ ConsoleApp/Aula2.cs
@@ -3,6 +3,40 @@ public static class Aula2
 {
     public static void Demo1()
     {
+        WriteLine("DEMO 1");
+        int a = 5;
+        Adicionar20(ref a);
+        WriteLine($"O valor de A é: {a}");
+    }
 
+    public static void Demo2()
+    {
+        WriteLine("DEMO 2");
+        string[] nomes = new string[]{"José", "Maria", "Jorge", "Cláudia", "Luana", "Mário"};
+
+        WriteLine("Digite o nome a ser substituído");
+        string atual = ReadLine();
+        WriteLine("Digite o nome substituto");
+        string novo = ReadLine();
+
+        SubstituirNome(nomes, atual, novo);
+
+        WriteLine(string.Join(", ", nomes));
+    }
+
+    static async void SubstituirNome(string[] nomes, string nomeAtual, string nomeNovo)
+    {
+        for(int i = 0; i < nomes.Length; i++)
+        {
+            if(nomes[i] == nomeAtual)
+            {
+                nomes[i] = nomeNovo;
+            }
+        }
+    }
+
+    private static void Adicionar20(ref int a)
+    {
+        a += 20;
     }
 }
--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,8 +1,13 @@
-﻿Aula1.Demo1();
+﻿using static System.Console;
+
+WriteLine("AULA 1");
+Aula1.Demo1();
 Aula1.Demo2();
 Aula1.Demo3();
 Aula1.Demo4();
 Aula1.Demo5a();
 Aula1.Demo5b();
 
+WriteLine("AULA 1");
 Aula2.Demo1();
+Aula2.Demo2();
